ca188b7057274c5505b51289a88bf85d30660dde
Mihai Vari <mvari@sugarsync.com>
2014-11-28 11:56:44 +0200

    updates

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,16 +1,62 @@
-node 'puppet-agent-r10k'{
-    class{'monitoring::zabbix::server':
-		zabbix_server => '192.168.150.56',
-		listen_port   => '8080',
-		db_password   => 'zabbix@pass#*'
-}
+node default {
 }
 
-node 'puppet-r10k'{
-    class{'monitoring::zabbix::server':
-                zabbix_server => '192.168.150.56',
-                listen_port   => '8080',
-                db_password   => 'zabbix@pass#*'
+node /^cmgmt01p(1|2)/ {
+  #class{ 'mrepo': }
+  #class{ 'puppetdb::database::postgresql':
+  #  listen_addresses       => 'localhost',
+  #  manage_firewall => false,
+  #}
+  #class{ 'puppetdb::server':
+  #  database_host          => 'localhost',
+  #  listen_address         => '0.0.0.0',
+  #  ssl_listen_address     => '0.0.0.0',
+  #  manage_firewall => false,
+  #}
+  #class{ 'puppet':
+  #  mode            => 'server',
+  #  db              => 'puppetdb',
+  #  db_port         => '8081',
+  #  db_server       => 'cpuppetdb01.corp.sharpcast.com',
+  #  db_user         => 'puppetdb',
+  #  db_password     => 'md53cbf124486f5dca866b9eb0d6a3bb314',
+  #  ca_authority    => true,
+  #  ca_server       => 'puppet.corp.sharpcast.com',
+  #  server_certname => 'puppet.corp.sharpcast.com',
+  #  autosign        => true,
+  #  passenger       => true,
+  #  require         => Class['yum'],
+  #  my_class        => 'puppet::server::data',
+  #}
+  #Class['puppetdb::database::postgresql'] -> Class['puppetdb::server'] -> Class['puppet']
+  class { 'r10k':
+    sources       => {
+      'puppet'    => {
+        'remote'  => 'https://stash.corp.sharpcast.com:8443/scm/ops/environments.git',
+        'basedir' => "${::settings::confdir}/environments",
+        'prefix'  => false,
+      },
+      'hiera'     => {
+        'remote'  => 'https://bob.com',
+        'basedir' => "${::settings::confdir}/hieradata",
+        'prefix'  => false,
+      }
+    },
+    purgedirs         => "${::settings::confdir}/environments",
+    manage_modulepath => true,
+    modulepath        => "${::settings::confdir}/environments/\$environment/modules",
+  }
 }
+node /^cpuppetdb01/ {
+  class{ 'postgresql::globals': version => '9.2', manage_firewall => false, }
+  class{ 'puppetdb::database::postgresql':
+    listen_addresses       => 'localhost',
+    manage_firewall => false,
+  }
+  class{ 'puppetdb::server':
+    database_host          => 'localhost',
+    ssl_listen_address     => '0.0.0.0',
+    listen_address         => '0.0.0.0',
+    manage_firewall => false,
+  }
 }
-
--- manifests/site.pp
+++ manifests/site.pp
@@ -1,1 +1,70 @@
-import "nodes"
+# /etc/puppet/manifests/site.pp
+#
+
+if versioncmp($::puppetversion,'3.6.1') >= 0 {
+
+  $allow_virtual_packages = hiera('allow_virtual_packages',false)
+
+  Package {
+    allow_virtual => $allow_virtual_packages,
+  }
+}
+
+$nagios_baseservices_template = 'monitoring/nagios/baseservices.erb'
+
+if ( $hostname == 'devint1' ) {
+  $cluster = 'devint1'
+} elsif ($hostname == 'devint2' ) {
+  $cluster = 'devint2'
+} elsif ($hostname == 'devint3' ) {
+  $cluster = 'devint3'
+} elsif ($hostname == 'devint4' ) {
+  $cluster = 'devint4'
+} elsif ( $cluster_type == 'build-test' ) {
+  $cluster = 'buildtest'
+} elsif ( $hostname =~ /qa1|qa-search01|qa-msg-q01/ ) {
+  $cluster = 'qa1'
+} elsif ( $hostname =~ /qa2|qa-search02|qa-msg-q02/ ) {
+  $cluster = 'qa2'
+} elsif ( $hostname =~ /qa3vm|apiqa3vm/ ) {
+  $cluster = 'qa3vm'
+} elsif ( $hostname =~ /qa3$|qa-search03|qa-msg-q03/ ) {
+  $cluster = 'qa3'
+} elsif ( $hostname =~ /qa4|qa-search04|qa-msg-q04/ ) {
+  $cluster = 'qa4'
+} elsif ( $hostname =~ /dpuppet|dvm|qamonitor/ ) {
+  $cluster = 'dev'
+} elsif ( $hostname =~ /puppet-slave-r10k/ ) {
+  $cluster = 'test'
+} else {
+  $cluster = 'c'
+}
+
+# The filebucket option allows for file backups to the server
+filebucket { main: server => 'puppet'}
+
+# don't distribute version control metadata
+File { ignore => ['\.svn', '\.git', 'CVS' ] }
+
+# Set global defaults - including backing up all files to the main filebucket and adds a global path
+File { backup => main }
+Exec { path   => "/usr/bin:/usr/sbin/:/bin:/sbin" }
+
+# We don't put anything in classes
+# import "classes/*"
+
+# Load up site specific zones
+#import "zones"
+
+# run stages
+stage { [pre, post]: }
+Stage[pre] -> Stage[main] -> Stage[post]
+#
+#include stdlib
+#include concat::setup
+# Load up nodes
+#import 'nodes'
+# Load classes from hiera
+hiera_include('classes')
+# Load resources from hiera
+hiera_resources('resources')
