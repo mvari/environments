96d9dbf693d8db10620252f0cd7bf2e06365f4e6
mvari <mihai.vari@gmail.com>
2014-12-19 12:57:06 +0200

    Update nodes.pp

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -7,10 +7,6 @@ node 'puppet-agent-r10k'{
 }
 
 node 'puppet-r10k'{
-    class{'monitoring::zabbix::server':
-                zabbix_server => '192.168.150.56',
-                listen_port   => '8080',
-                db_password   => 'zabbix@pass#*'
-}
+class { 'nagios': }
 }
 
